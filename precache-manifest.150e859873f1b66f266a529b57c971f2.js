09b73038c65f5acf3cdf0e570f892b9366b9f049
nnt25251325 <nguyennhutinh18@gmail.com>
2018-11-30 18:38:54 +0700

    CREATE P

---
+++ precache-manifest.150e859873f1b66f266a529b57c971f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06a22a6c5e9853016389",
+    "url": "/static/js/main.06a22a6c.chunk.js"
+  },
+  {
+    "revision": "f8c94388021c1341eada",
+    "url": "/static/js/1.f8c94388.chunk.js"
+  },
+  {
+    "revision": "06a22a6c5e9853016389",
+    "url": "/static/css/main.3595967c.chunk.css"
+  },
+  {
+    "revision": "cb7a930c7a70c6f0b6a9359068b9b559",
+    "url": "/index.html"
+  }
+];
